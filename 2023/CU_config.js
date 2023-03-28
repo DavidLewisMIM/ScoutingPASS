da2ef436da5908d33adc1f4943be84c8b0b844d7
DavidLewisMIM <122945414+DavidLewisMIM@users.noreply.github.com>
2023-03-28 18:51:42 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023schar",
+      "defaultValue": "2023pchdis",
       "required": "false",
       "disabled": "false"
     },
