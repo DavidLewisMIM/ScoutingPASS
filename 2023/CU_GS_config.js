f648043c9ded0f6c7813d3e64fc2f14d236270ee
DavidLewisMIM <122945414+DavidLewisMIM@users.noreply.github.com>
2023-04-20 08:56:43 -0400

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023schar",
+      "defaultValue": "2023cur",
       "required": "true",
       "disabled": "true"
     },
